ad400943ce3bf05eff90beb04013449a17a126aa
TERRY <terryknowsall@gmail.com>
2016-04-20 00:00:14 +0100

    Update app_scope.js

--- app_scope.js
+++ app_scope.js
@@ -2,9 +2,12 @@
   'use strict';
   alert('test inside ');
 angular.module('scopeExample', []).controller('MyController', ['$scope', function($scope) {
+  
+  alert('test inside 1');
     $scope.username = 'World';
 
     $scope.sayHello = function() {
+  alert('test inside 2');
       $scope.greeting = 'Hello ' + $scope.username + '!';
     };
   }]);
