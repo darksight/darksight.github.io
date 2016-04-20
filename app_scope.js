2fefd77daa1f7af18d3af20ad5fef823f843ec9d
TERRY <terryknowsall@gmail.com>
2016-04-20 15:04:17 +0100

    Update app_scope.js

--- app_scope.js
+++ app_scope.js
@@ -1,11 +1,11 @@
 var myApp = angular.module('scopeExample', [])
   .controller('MyController', ['$scope', function($scope) {
-    //alert('test inside 1_1');
+    alert('test inside 1_1');
     $scope.username = 'World';
 
     $scope.sayHello = function() {
       
-      //alert('test inside 1_2');
+      alert('test inside 1_2');
       $scope.greeting = 'Hello ' + $scope.username + '!';
     };
   }]);
